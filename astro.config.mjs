91bdefd29fa7ef31441b7954512f5a112d85f17b
Eric M Greeff <emgreeff@datadash.cloud>
2023-09-27 13:07:27 -0600

    modified:   .gitignore 	new file:   .vercel/output/config.json 	new file:   .vercel/output/functions/render.func/.vc-config.json 	new file:   .vercel/output/functions/render.func/package.json 	new file:   .vercel/output/functions/render.func/usr/bin/ldd 	new file:   .vercel/output/static/_astro/client.401dfeec.js 	new file:   .vercel/output/static/_astro/client.ef651e53.js 	new file:   .vercel/output/static/_astro/index.979e1096.css 	new file:   .vercel/output/static/favicon.svg 	modified:   astro.config.mjs 	new file:   jsconfig.json 	modified:   package-lock.json 	modified:   package.json 	renamed:    src/components/CandleSticks.astro -> src/components/CandleSticks.jsx 	new file:   svelte.config.js 	new file:   tailwind.config.cjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), svelte(), tailwind(), mdx()],
+  output: "server",
+  adapter: vercel()
 });
